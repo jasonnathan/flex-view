aa6262e5d04427d76217932f9517b578c8a2daf1
elnebuloso <jan.thoennessen@googlemail.com>
2014-07-18 20:58:03 +0200

    moved from flex

---
+++ src/Flex/View/PhtmlView.php
@@ -0,0 +1,126 @@
+<?php
+namespace Flex\View;
+
+use Flex\View\Exception\ViewException;
+
+/**
+ * Class PhtmlView
+ *
+ * @package Flex\View
+ * @author Jeff Tunessen <jeff.tunessen@gmail.com>
+ */
+class PhtmlView extends AbstractView implements ViewInterface {
+
+    /**
+     * @var string
+     */
+    private $basePath;
+
+    /**
+     * @var string
+     */
+    private $template;
+
+    /**
+     * @return string
+     */
+    public function getBasePath() {
+        return $this->basePath;
+    }
+
+    /**
+     * @param $basePath
+     * @throws ViewException
+     */
+    public function setBasePath($basePath) {
+        $this->basePath = trim($basePath);
+
+        if(empty($this->basePath)) {
+            throw new ViewException("missing base path");
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplate() {
+        return $this->template;
+    }
+
+    /**
+     * @param string $template
+     * @throws ViewException
+     */
+    public function setTemplate($template) {
+        $this->template = trim($template);
+
+        if(empty($this->template)) {
+            throw new ViewException("missing template");
+        }
+    }
+
+    /**
+     * @return string
+     * @throws ViewException
+     */
+    public function getBody() {
+        if(is_null($this->getBasePath())) {
+            throw new ViewException("missing view base path");
+        }
+
+        if(is_null($this->getTemplate())) {
+            throw new ViewException("missing template");
+        }
+
+        $path = "{$this->getBasePath()}/{$this->getTemplate()}";
+
+        if(!realpath($path)) {
+            throw new ViewException("view not found: {$path}");
+        }
+
+        $data = array();
+
+        foreach($this->getData() as $name => $value) {
+            $data[$name] = $value;
+
+            if($value instanceof PhtmlView) {
+                $data[$name] = $value->getBody();
+            }
+        }
+
+        extract($data, EXTR_REFS);
+        ob_start();
+
+        include $path;
+
+        return ob_get_clean();
+    }
+
+    /**
+     * @return array
+     */
+    public function getResponseHeaders() {
+        return array(
+            'Content-Type' => 'text/html'
+        );
+    }
+
+    /**
+     * @param string $template
+     * @return string
+     * @throws ViewException
+     */
+    public function render($template) {
+        $path = "{$this->getBasePath()}/{$template}";
+
+        if(!realpath($path)) {
+            throw new ViewException("view not found: {$path}");
+        }
+
+        ob_start();
+
+        include $path;
+
+        return ob_get_clean();
+    }
+}
---
+++ tests/src/FlexTest/View/PhtmlViewTest.php
@@ -0,0 +1,158 @@
+<?php
+namespace FlexTest\View;
+
+use Flex\View\PhtmlView;
+
+/**
+ * Class PhtmlViewTest
+ *
+ * @package FlexTest\View
+ * @author Jeff Tunessen <jeff.tunessen@gmail.com>
+ */
+class PhtmlViewTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var PhtmlView
+     */
+    private $view;
+
+    /**
+     * @return void
+     */
+    public function setUp() {
+        $this->view = new PhtmlView();
+    }
+
+    /**
+     * @return void
+     */
+    public function tearDown() {
+        $this->view = null;
+    }
+
+    /**
+     * @return void
+     */
+    public function test_setBasePath() {
+        $expexted = 'foo/bar/baz';
+        $this->view->setBasePath($expexted);
+        $this->assertEquals($expexted, $this->view->getBasePath());
+    }
+
+    /**
+     * @return void
+     * @expectedException \Flex\View\Exception\ViewException
+     * @expectedExceptionMessage missing base path
+     */
+    public function test_setBasePathEmpty() {
+        $this->view->setBasePath('  ');
+    }
+
+    /**
+     * @return void
+     */
+    public function test_setTemplate() {
+        $expexted = 'template';
+        $this->view->setTemplate($expexted);
+        $this->assertEquals($expexted, $this->view->getTemplate());
+    }
+
+    /**
+     * @return void
+     * @expectedException \Flex\View\Exception\ViewException
+     * @expectedExceptionMessage missing template
+     */
+    public function test_setTemplateEmpty() {
+        $this->view->setTemplate('  ');
+    }
+
+    /**
+     * @return void
+     * @expectedException \Flex\View\Exception\ViewException
+     * @expectedExceptionMessage missing view base path
+     */
+    public function test_getBodyInvalidBasePath() {
+        $this->view->getBody();
+    }
+
+    /**
+     * @return void
+     * @expectedException \Flex\View\Exception\ViewException
+     * @expectedExceptionMessage missing template
+     */
+    public function test_getBodyInvalidTemplate() {
+        $this->view->setBasePath('tests/resources');
+        $this->view->getBody();
+    }
+
+    /**
+     * @return void
+     * @expectedException \Flex\View\Exception\ViewException
+     */
+    public function test_getBodyInvalidView() {
+        $this->view->setBasePath('tests/resources');
+        $this->view->setTemplate('views/script-not-found.phtml');
+        $this->view->getBody();
+    }
+
+    /**
+     * @return void
+     */
+    public function test_getBody() {
+        $this->view->setBasePath('tests/resources');
+        $this->view->setTemplate('views/unit-testing/profile-settings.phtml');
+        $this->view->name = 'foo';
+        $this->assertEquals('<h2>user profile</h2><h3>foo</h3>', $this->view->getBody());
+    }
+
+    /**
+     * @return void
+     */
+    public function test_getBodyLayout() {
+        $layout = new PhtmlView();
+        $layout->setBasePath('tests/resources');
+        $layout->setTemplate('layouts/unit-testing.phtml');
+
+        $view = new PhtmlView();
+        $view->setBasePath('tests/resources');
+        $view->setTemplate('views/unit-testing/profile-settings.phtml');
+        $view->name = 'foo';
+
+        $layout->content = $view;
+        $this->assertEquals('<h1>unit test layout</h1><h2>user profile</h2><h3>foo</h3>', $layout->getBody());
+    }
+
+    /**
+     * @return void
+     */
+    public function test_getResponseHeaders() {
+        $expected = array(
+            'Content-Type' => 'text/html'
+        );
+
+        $this->assertEquals($expected, $this->view->getResponseHeaders());
+    }
+
+    /**
+     * @return void
+     */
+    public function test_render() {
+        $layout = new PhtmlView();
+        $layout->setBasePath('tests/resources');
+        $layout->setTemplate('layouts/unit-testing.phtml');
+
+        $this->assertEquals('<h2>navigation</h2>', (string) $layout->render('views/unit-testing/navigation.phtml'));
+    }
+
+    /**
+     * @return void
+     * @expectedException \Flex\View\Exception\ViewException
+     */
+    public function test_renderInvalid() {
+        $layout = new PhtmlView();
+        $layout->setBasePath('tests/resources');
+        $layout->setTemplate('layouts/unit-testing.phtml');
+
+        $layout->render('views/unit-testing/script-not-found.phtml');
+    }
+}
